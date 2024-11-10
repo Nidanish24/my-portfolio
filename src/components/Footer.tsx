b3c171e585a20b5f1a919b1604978564d15b8547
Nidanish24 <nida.nd24@gmail.com>
2024-11-10 23:53:17 +0500

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+      <footer className="bg-[#0a0908] text-[#a97da9] py-6 text-center text-md lg:text-2xl font-[Caveat]">© 2024 Nida Danish. All rights reserved.</footer>
+    </>
+  )
+}
+
+export default Footer
